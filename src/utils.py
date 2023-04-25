a19d85b89a2b776c6279ab6782c1da51a15bfb92
Tathagat Saha <99318598+tatz210588@users.noreply.github.com>
2023-04-26 00:15:44 +0200

    comm

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,8 @@ import pandas as pd
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split,GridSearchCV
+import pickle
 
 
 def save_object(file_path, obj):
@@ -20,12 +21,18 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_model(x_train,y_train,x_test,y_test, models):
+def evaluate_model(x_train,y_train,x_test,y_test, models, param):
     try:
         report={}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+            
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train) #train model
 
             y_train_pred = model.predict(x_train)
@@ -38,5 +45,12 @@ def evaluate_model(x_train,y_train,x_test,y_test, models):
         
         return report
 
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
